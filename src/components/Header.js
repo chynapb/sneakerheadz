3ec86fd7bc997a8732be44f226d24e892aab68fc
Chyna <chynapblair@gmail.com>
2023-08-07 19:37:05 -0400

    Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <h1>
+    <h1 className='header'>
       Sneaker<span style={{ color: 'rgb(49, 49, 49)' }}>Headz</span>
     </h1>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
 import { Button, Container, Nav, Navbar as NavbarBs } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 export const Navbar = () => {
   return (
     <NavbarBs sticky='top' className='bg-dark shadow-sm mb-3'>
       <Container>
-        <Button>Cart</Button>
+        <Button
+          style={{
+            width: '3rem',
+            height: '3rem',
+            position: 'relative',
+            border: 'none',
+          }}
+          variant='outline-primary'
+          className='rounded-circle'
+        >
+          <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
+        </Button>
       </Container>
     </NavbarBs>
   );
