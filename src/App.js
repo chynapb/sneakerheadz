3de2b57c19585bcc532fc8823f764fdac504aa93
Chyna <chynapblair@gmail.com>
2023-08-10 18:54:36 -0400

    Filter component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { Shop } from './pages/Shop';
 import './App.css';
+import { Filter } from './components/Filter';
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Filter />
       <Container className='mb-3'>
         <Routes>
           <Route path='/' element={<Shop />} />
---
+++ src/components/Filter.js
@@ -0,0 +1,7 @@
+export const Filter = () => {
+  return (
+    <div>
+      <h5>Filter Items</h5>
+    </div>
+  );
+};
