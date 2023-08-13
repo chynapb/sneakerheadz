196b2dc73ff68147b85aaa1bfe68a4dc229bce6c
Chyna <chynapblair@gmail.com>
2023-08-13 08:55:45 -0400

    Local storage

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,15 +1,24 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { ShoppingCart } from '../components/ShoppingCart';
 
+// Create context
 const CartContext = createContext({});
+
+// Store cart items in a variable for local storage
+const storedItems = JSON.parse(localStorage.getItem('cart-items') || '[]');
 
 export const useCart = () => {
   return useContext(CartContext);
 };
 
 export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(storedItems);
   const [isOpen, setIsOpen] = useState(false);
+
+  // Add items to local storage
+  useEffect(() => {
+    localStorage.setItem('cart-items', JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const openCart = () => setIsOpen(true);
   const closeCart = () => setIsOpen(false);
