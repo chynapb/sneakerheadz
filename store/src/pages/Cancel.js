273e79b2ef64a8469ea17d23a78de3507e290278
Chyna <chynapblair@gmail.com>
2023-08-20 08:46:31 -0400

    Styling

--- store/src/pages/Cancel.js
+++ store/src/pages/Cancel.js
@@ -1,7 +1,8 @@
 export const Cancel = () => {
   return (
     <h3>
-      Still looking? <br /> Cick on "SHOP" to browse our entire collection!
+      Still looking around? <br /> Select "SHOP" to browse our entire
+      collection!
     </h3>
   );
 };
