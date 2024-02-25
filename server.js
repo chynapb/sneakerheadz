41c926bb3fa5b6d9b61b121a44a9617188dfe40d
Chyna <chynapblair@gmail.com>
2024-02-25 15:51:42 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -3,19 +3,13 @@ const cors = require('cors');
 const stripe = require('stripe')(
   'sk_test_51NgogfHSrOGQgt9k8BAfm9WeCLh4iBdCKOskIdLDqo4MYROQAMaCPQKGcHkBZCMMtTKa4tx64seKGMWz7ufk1GNB00wMk5jjvW'
 );
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.static('public'));
 app.use(express.json());
-
-// Serve files from public directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-app.use(express.static(join(__dirname, 'public')));
 
 app.post('/checkout', async (req, res) => {
   // Create line items and format them for Stripe
