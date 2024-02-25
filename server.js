5852a963763019624a7e2a8a5b8c950f46417894
Chyna <chynapblair@gmail.com>
2024-02-25 17:00:34 -0500

    Updated files for Heroku

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const stripe = require('stripe')(
   'sk_test_51NgogfHSrOGQgt9k8BAfm9WeCLh4iBdCKOskIdLDqo4MYROQAMaCPQKGcHkBZCMMtTKa4tx64seKGMWz7ufk1GNB00wMk5jjvW'
 );
+require('dotenv').config({ path: './config.env' });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static('store/public'));
 app.use(express.json());
+
+// Serve React app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.post('/checkout', async (req, res) => {
   // Create line items and format them for Stripe
